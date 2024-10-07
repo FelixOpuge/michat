d8af986d8668fd2f2c6f22d019e84908f9ec3a29
Felix Opuge <omondifelix702@gmail.com>
2024-10-07 23:02:42 +0300

    Authentication UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import { Button } from "@/components/ui/button";
+import AuthScreen from "@/features/auth/components/AuthScreen";
+
 
 export default function Home() {
   return (
-    <div>
-      <Button>
-        Click me
-      </Button>
-    </div>
+    <>
+      <AuthScreen />
+    </>
   );
 }
---
+++ src/features/auth/components/SignUpCard.tsx
@@ -0,0 +1,95 @@
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Separator } from '@/components/ui/separator'
+import React, { useState } from 'react'
+import { FcGoogle } from 'react-icons/fc'
+import { FaFacebook } from 'react-icons/fa'
+import { SignInFlow } from '../authTypes'
+
+interface SignUpProps {
+  setState: (state: SignInFlow) => void
+}
+
+const SignUpCard = ({ setState }: SignUpProps) => {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  return (
+    <Card className='h-full w-full p-8'>
+        <CardHeader className='px-0 pt-0'>
+          <CardTitle>
+            Register to continue
+          </CardTitle>
+          <CardDescription>
+          Use your email or another service to continue
+          </CardDescription>
+        </CardHeader>
+        <CardContent className='space-y-5 px-0 pb-0'>
+          <form action="" className='space-y-2.5'>
+            <Input
+              disabled={false}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder='Email'
+              type='email'
+              required
+            />
+            <Input
+              disabled={false}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder='Password'
+              type='password'
+              required
+            />
+            <Input
+              disabled={false}
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              placeholder='Confirm Password'
+              type='password'
+              required
+            />
+            <Button
+              type= 'submit'
+              className='w-full'
+              size='lg'
+              disabled={false}
+            >
+              Login
+            </Button>
+          </form>
+          <Separator />
+          <div className='flex flex-col gap-y-3'>
+            <Button
+              variant='outline'
+              className='w-full relative'
+              size='lg'
+              disabled={false}
+              onClick={() => {}}
+            >
+              <FcGoogle className='size-5 absolute top-2.5 left-2.5' />
+              Login with google
+            </Button>
+            <Button
+              variant='outline'
+              className='w-full relative'
+              size='lg'
+              disabled={false}
+              onClick={() => {}}
+            >
+              <FaFacebook className='size-5 absolute top-2.5 left-2.5 text-sky-700' />
+              Login with facebook
+            </Button>
+          </div>
+          <p className='text-xs text-muted-foreground'>
+            Already have an account? <span onClick={() =>setState('signIn')} className='text-sky-700 cursor-pointer hover:underline'>Sign In</span> 
+          </p>
+        </CardContent>
+    </Card>
+  )
+}
+
+export default SignUpCard
